5dc8bddfe4b61aa35d7a6f61f031eac8e8381f11
m2nsp <mini071663@gmail.com>
2025-05-14 15:19:22 +0900

    ✨fit: backend-init

---
+++ backend/src/test/java/com/xclone/backend/XcloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xclone.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XcloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
