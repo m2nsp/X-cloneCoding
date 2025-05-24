f6ce4b4ec2281a6280fdad21454a0d9782b6ba6f
m2nsp <mini071663@gmail.com>
2025-05-24 21:47:56 +0900

    ⚙️배포 관련 설정 및 오류 해결
    issue: #3

--- backend/src/main/java/com/xclone/backend/XcloneApplication.java
+++ backend/src/main/java/com/xclone/backend/XcloneApplication.java
@@ -2,8 +2,12 @@ package com.xclone.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaRepositories(basePackages = "com.xclone.backend.repository")
+@EntityScan(basePackages = "com.xclone.backend.entity")
 @EnableJpaAuditing
 @SpringBootApplication
 public class XcloneApplication {
